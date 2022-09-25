c9640de09e3aec16a6f93eaa47fc10e716614ed9
k.petelski <kai.pet96@viidelta.com>
2022-09-26 00:24:11 +0200

    iorwf added

--- Instructions/ByteOrientedInstructions/iorwf.cs
+++ Instructions/ByteOrientedInstructions/iorwf.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace picsim.Instructions.ByteOrientedInstructions
 {
-    internal class iorwf
+    internal class iorwf : ByteOrientedInstructions
     {
+        public iorwf(int instruction, Pic uc)
+        {
+            _instruction = instruction;
+            _pic = uc;
+        }
+
+        public override void Decode()
+        {
+            _f = _instruction & _fBitmask;
+            _d = _instruction & _dBitmask;
+        }
+
+        public override void Execute()
+        {
+            Decode();
+            var register = _pic.GetByte(_f);
+            var result = register | _pic.Wreg;
+            _pic.WriteResult(_d,_f,result);
+            _pic.IncProgCounter(false);
+            _pic.ZFlag(result);
+        }
     }
 }
