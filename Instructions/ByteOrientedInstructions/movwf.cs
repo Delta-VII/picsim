d5de772a4adbcb1bc077e5cec97953cf8728a12e
k.petelski <kai.pet96@viidelta.com>
2022-09-26 00:29:53 +0200

    movwf added

--- Instructions/ByteOrientedInstructions/movwf.cs
+++ Instructions/ByteOrientedInstructions/movwf.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace picsim.Instructions.ByteOrientedInstructions
 {
-    internal class movwf
+    internal class movwf : ByteOrientedInstructions
     {
+        public movwf()
+        {
+        }
+
+        public override void Decode()
+        {
+            _f = _instruction & _fBitmask;
+            _d = _instruction & _dBitmask;
+        }
+
+        public override void Execute()
+        {
+            _pic.WriteByte(_f,_pic.Wreg);
+            _pic.IncProgCounter(false);
+        }
     }
 }
