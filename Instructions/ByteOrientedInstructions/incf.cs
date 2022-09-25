89b6785ceeb107e8d3b3a80b841de63e84f974cf
k.petelski <kai.pet96@viidelta.com>
2022-09-26 00:19:21 +0200

    incf added

--- Instructions/ByteOrientedInstructions/incf.cs
+++ Instructions/ByteOrientedInstructions/incf.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace picsim.Instructions.ByteOrientedInstructions
 {
-    internal class incf
+    internal class incf : ByteOrientedInstructions
     {
+        public incf(int instruction, Pic uc)
+        {
+            _instruction = instruction;
+            _pic = uc;
+        }
+
+        public override void Decode()
+        {
+            _f = _instruction & _fBitmask;
+            _d = _instruction & _dBitmask;
+        }
+
+        public override void Execute()
+        {
+            Decode();
+            var register = _pic.GetByte(_f);
+            var result = register + 1;
+            _pic.WriteResult(_d,_f,result);
+            _pic.IncProgCounter(false);
+            _pic.ZFlag(result);
+        }
     }
 }
