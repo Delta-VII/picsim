763163d472d30b63ca7085339aa93ebbbed9cd9f
k.petelski <kai.pet96@viidelta.com>
2022-09-25 23:15:09 +0200

    comf added

--- Instructions/ByteOrientedInstructions/clrw.cs
+++ Instructions/ByteOrientedInstructions/clrw.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace picsim.Instructions.ByteOrientedInstructions
 {
-    internal class clrw
+    internal class clrw : ByteOrientedInstructions
     {
+        public clrw(int instruction, Pic uc)
+        {
+            _instruction = instruction;
+            _pic = uc;
+        }
+        public override void Decode()
+        {
+            _f = _instruction & _fBitmask;
+            _d = _instruction & _dBitmask;
+        }
+
+        public override void Execute()
+        {
+            Decode();
+            _pic.Wreg = 0;
+            _pic.SetZFlag(true);
+            _pic.IncProgCounter(false);
+        }
     }
 }
