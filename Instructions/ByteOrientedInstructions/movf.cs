f402cef095040c68215af4bb6f167e619685d5ca
k.petelski <kai.pet96@viidelta.com>
2022-09-26 00:27:40 +0200

    movf added

--- Instructions/ByteOrientedInstructions/movf.cs
+++ Instructions/ByteOrientedInstructions/movf.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace picsim.Instructions.ByteOrientedInstructions
 {
-    internal class movf
+    internal class movf : ByteOrientedInstructions
     {
+        public movf(int instruction, Pic uc)
+        {
+            _instruction = instruction;
+            _pic = uc;
+        }
+
+        public override void Decode()
+        {
+            _f = _instruction & _fBitmask;
+            _d = _instruction & _dBitmask;
+        }
+
+        public override void Execute()
+        {
+            var result = _pic.GetByte(_f);
+            _pic.WriteResult(_d,_f,result);
+            _pic.IncProgCounter(false);
+            _pic.ZFlag(result); 
+        }
     }
 }
