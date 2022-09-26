b68c300974a6e05e73a0e360cd4b38d56ffc8074
k.petelski <kai.pet96@viidelta.com>
2022-09-27 00:52:50 +0200

    andwf added

--- Instructions/ByteOrientedInstructions/andwf.cs
+++ Instructions/ByteOrientedInstructions/andwf.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace picsim.Instructions.ByteOrientedInstructions
 {
-    internal class andwf
+    internal class andwf : ByteOrientedInstructions
     {
+        public andwf(int instruction, Pic uc)
+        {
+            _instruction = instruction;
+            _pic = uc;
+        }
+
+        public override void Decode()
+        {
+            _f = _instruction & _fBitmask;
+            _d = _instruction & _dBitmask;
+        }
+
+        public override void Execute()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
