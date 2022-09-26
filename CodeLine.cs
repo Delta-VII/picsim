b09834ca754bc988c22549e535d1fd383c559bc7
k.petelski <kai.pet96@viidelta.com>
2022-09-26 18:52:00 +0200

    lol

---
+++ CodeLine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace picsim
+{
+    public class CodeLine
+    {
+        public int LineNumber { get; set; }
+        public string Code { get; set; }
+        public bool IsBreakPoint { get; set; }
+        public bool IsActiveLine { get; set; }
+
+        public CodeLine(string line)
+        {
+            Code = line;
+            LineNumber = 1;
+            IsActiveLine = false;
+            IsBreakPoint = false;
+        }
+    }
+}
---
+++ Instructions/Instruction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace picsim.Instructions
+{
+    internal abstract class Instruction
+    {
+        protected int _instruction;
+        protected Pic _pic;
+        public abstract void Decode();
+
+        public abstract void Execute();
+    }
+}
