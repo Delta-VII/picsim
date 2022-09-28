ca13b78f3891dabbe6a31b4e661e8d4e04371321
Delta-VII <kai.pet96@viidelta.com>
2022-09-28 13:47:08 +0200

    lol

--- Instructions/ByteOrientedInstructions/swapf.cs
+++ Instructions/ByteOrientedInstructions/swapf.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace picsim.Instructions.ByteOrientedInstructions
 {
-    internal class swapf
+    internal class swapf : ByteOrientedInstructions
     {
+        public swapf(int instruction, Pic uc)
+        {
+            _instruction = instruction;
+            _pic = uc;
+        }
+
+        public override void Decode()
+        {
+            _f = _instruction & _fBitmask;
+            _d = _instruction & _dBitmask;
+        }
+
+        public override void Execute()
+        {
+            throw new NotImplementedException();
+            
+            (x & 0x0F) << 4 | (x & 0xF0) >> 4);
+        }
     }
 }
